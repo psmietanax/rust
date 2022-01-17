8ca15985079f3123222a178c71e487bd4c619e62
psmietanax <42338458+psmietanax@users.noreply.github.com>
2022-01-17 17:21:11 +0000

    Create fizzbuzz.rs

---
+++ concurrent/fizzbuzz.rs
@@ -0,0 +1,48 @@
+fn main() {
+    fizz_buzz(20);
+}
+
+fn fizz_buzz(n: usize) {
+    if n == 0 {
+        return;
+    }
+
+    let (sender_in, receiver_in) = mpsc::channel();
+    let (sender_out, receiver_out) = mpsc::channel();
+
+    thread::spawn(move || {
+        while let Some(num) = receiver_in.recv().unwrap() {
+            println!("[THREAD2]: {}", num_to_fizz_buzz(num));
+            if num == n {
+                sender_out.send(None);
+            } else {
+                sender_out.send(Some(num + 1));
+            }
+        }
+        sender_out.send(None);
+    });
+
+    sender_in.send(Some(1));
+
+    while let Some(num) = receiver_out.recv().unwrap() {
+        println!("[THREAD1]: {}", num_to_fizz_buzz(num));
+        if num == n {
+            sender_in.send(None);
+        } else {
+            sender_in.send(Some(num + 1));
+        }
+    }
+    sender_in.send(None);
+}
+
+fn num_to_fizz_buzz(n: usize) -> String {
+    if n % 15 == 0 {
+        "fizzbuzz".to_string()
+    } else if n % 3 == 0 {
+        "fizz".to_string()
+    } else if n % 5 == 0 {
+        "buzz".to_string()
+    } else {
+        n.to_string()
+    }
+}
