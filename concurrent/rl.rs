74c2a5d2eb7e341b4df926f2d501e971c9d19c30
psmietanax <42338458+psmietanax@users.noreply.github.com>
2022-03-27 21:24:00 +0100

    Update rl.rs

--- concurrent/rl.rs
+++ concurrent/rl.rs
@@ -1,42 +1,68 @@
-use std::{mem, thread};
-use std::ops::{Add, Div};
+use std::thread;
+use std::ops::{AddAssign, Div};
 use std::sync::{Arc, Mutex};
 use std::time::{Duration, Instant};
+use chrono::{Local, DateTime};
+
+static SECS: u64 = 5;
+static MAX_REQS: u32 = 100;
+static REFRESH_SECS: u64 = 1;
 
 fn main() {
-    LeakyBucket::new(100, Duration::from_secs(10), Duration::from_secs(1));
+    let lb = LeakyBucket::new(MAX_REQS, Duration::from_secs(REFRESH_SECS));
+    let result = Arc::new(Mutex::new(Vec::new()));
+
+    (0..4).for_each(|_| {
+        let lbb = lb.clone();
+        let ret = result.clone();
+        thread::spawn(move || {
+            loop {
+                if lbb.acquire() {
+                    ret.lock().unwrap().push(1);
+                }
+            }
+        });
+    });
+
+    thread::sleep(Duration::from_secs(SECS));
+
+    let counter = result.lock().unwrap().iter().count();
+
+    println!("PERMITS: {}, allowed ~ {}", counter, MAX_REQS as u64 * SECS);
 }
 
 struct LeakyBucket {
     next_available_time: Arc<Mutex<Instant>>,
-    wait_interval: Duration,
-    timeout: Duration
+    wait_interval: Duration
 }
 
-impl LeakyBucket {
-    fn acquire(&self) -> bool {
-        {
-            let next_available_time = self.next_available_time.lock().unwrap();
-            if Instant::now() >= *next_available_time {
-                print!("yeah");
-                *next_available_time.add(self.wait_interval);
-            } else {
-                mem::drop(next_available_time);
-                thread::park_timeout()
-            }
+impl Clone for LeakyBucket {
+    fn clone(&self) -> Self {
+        LeakyBucket {
+            next_available_time: Arc::clone(&self.next_available_time),
+            wait_interval: self.wait_interval.clone()
         }
-        true
-
     }
 }
 
 impl LeakyBucket {
-    fn new(max_request: u32, refresh_interval: Duration, timeout: Duration) -> Self {
+    fn acquire(&self) -> bool {
+        let mut next_available_time = self.next_available_time.lock().unwrap();
+        if Instant::now() >= *next_available_time {
+            next_available_time.add_assign(self.wait_interval);
+            true
+        } else {
+            false
+        }
+    }
+}
+
+impl LeakyBucket {
+    fn new(max_request: u32, refresh_interval: Duration) -> Self {
         let wait_interval = refresh_interval.div(max_request);
         LeakyBucket {
             next_available_time: Arc::new(Mutex::new(Instant::now())),
             wait_interval,
-            timeout
         }
     }
 }
